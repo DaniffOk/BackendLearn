a28dd75ecf90e9ec5583efdfab8be1f5cdfc12ad
Daniff <daniffok123@gmail.com>
2026-04-01 12:06:31 +0200

    add error handling

--- src/app.js
+++ src/app.js
@@ -6,4 +6,9 @@ const usersRouter = require("./routes/usersRouter");
 app.use(express.json());
 app.use("/users", usersRouter);
 
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+});
+
 module.exports = app;
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,35 +1,36 @@
 const usersService = require("../services/usersServiceDB");
+const asyncHandler = require("../errorHandler")
 
-const getAllUsers = async (req, res) =>{
+const getAllUsers = asyncHandler( async(req, res) =>{
     const users = await usersService.findAllUsers();
     return res.json(users);
-};
+});
 
-const getUserbyId = async (req, res) =>{
+const getUserbyId = asyncHandler( async(req, res) =>{
     const userId = Number(req.params.userId);
     const user = await usersService.findUserById(userId);
 
     if (!user) return res.status(404).json({ message: "user not found" });
     
     return res.json(user);
-};
+});
 
-const addUser = async (req, res) =>{
+const addUser = asyncHandler( async(req, res) =>{
     const { name, age } = req.body;
 
     if (!age||!name) return res.status(400).json({ mesage:"age and name required" });
 
     const user = await usersService.addUser({name, age});
     return res.status(201).json(user);
-};
+});
 
-const deleteUser = async (req, res) =>{
+const deleteUser = asyncHandler( async(req, res) =>{
     const userId = Number(req.params.userId);
 
     const user = await usersService.deleteUser(userId);
     if (!user) return res.status(404).json({ mesage: "user not found" });
 
     return res.status(200).json(user);
-}
+})
 
 module.exports = { getAllUsers, getUserbyId, addUser, deleteUser};
